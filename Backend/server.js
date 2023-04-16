ad12722efb580255107ed8785e38d05e11eb6c0d
Harshit <inbox.Harshityadav@gmail.com>
2023-04-16 19:08:29 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://zomato-harshit.netlify.app/'
+    origin: 'https://zomato-harshit.netlify.app'
 }));
 app.set('trust proxy', 1);
 
